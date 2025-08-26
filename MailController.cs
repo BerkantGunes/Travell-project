dc19ddbd9051d5bfec1050c5b0177de340aaa692
Berkant Gunes <gunesberkant96@gmail.com>
2025-08-26 17:56:47 +0300

    Add files via upload

---
+++ MailController.cs
@@ -0,0 +1,44 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Traveler.Models;
+
+namespace Traveler.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailBoxAddressFrom = new MailboxAddress("Admin", "travelertestproject@gmail.com");
+
+            mimeMessage.From.Add(mailBoxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("travelertestproject@gmail.com", "uzuraqrndrpjnwiq");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+    }
+}
