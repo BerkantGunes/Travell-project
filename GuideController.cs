6ea355cf10f5ed343f90f0f8ba5ff3821e6216be
Berkant Gunes <gunesberkant96@gmail.com>
2025-08-26 17:56:31 +0300

    Add files via upload

---
+++ GuideController.cs
@@ -0,0 +1,87 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Traveler.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Guide")] 
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+        [Route("AddGuide")]
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [Route("AddGuide")]
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            GuideValidator validationRules = new GuideValidator();
+            ValidationResult result = validationRules.Validate(guide);
+            if (result.IsValid)
+            {
+                _guideService.TAdd(guide);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+                return View();
+            }
+        }
+        [Route("UpdateGuide")]
+        [HttpGet]
+        public IActionResult UpdateGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            return View(values);
+        }
+        [Route("UpdateGuide")]
+        [HttpPost]
+        public IActionResult UpdateGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        [Route("DeleteGuide/{id}")]
+        public IActionResult DeleteGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            _guideService.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [Route("ChangeToTrue/{id}")]
+        public IActionResult ChangeToTrue(int id)
+        {
+            _guideService.TChangeToTrueByGuide(id);
+            return RedirectToAction("Index", "Guide", new {area="Admin"});
+        }
+        [Route("ChangeToFalse/{id}")]
+        public IActionResult ChangeToFalse(int id)
+        {
+            _guideService.TChangeToFalseByGuide(id);
+            return RedirectToAction("Index", "Guide", new {area="Admin"});
+        }
+    }
+}
